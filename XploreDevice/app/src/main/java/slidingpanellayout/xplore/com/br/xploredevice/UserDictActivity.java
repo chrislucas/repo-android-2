fee5423324ee7fbc9e9b9ba63286ed50efd4556b
chrislucas <christoffer.luccas@gmail.com>
2018-01-26 17:44:12 -0200

    brincando com UserDictonary content provider

--- XploreDevice/app/src/main/java/slidingpanellayout/xplore/com/br/xploredevice/UserDictActivity.java
+++ XploreDevice/app/src/main/java/slidingpanellayout/xplore/com/br/xploredevice/UserDictActivity.java
@@ -1,7 +1,10 @@
 package slidingpanellayout.xplore.com.br.xploredevice;
 
+import android.provider.UserDictionary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import slidingpanellayout.xplore.com.br.xploredevice.utils.UserDictContentProvider;
 
 public class UserDictActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class UserDictActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dict);
+        UserDictContentProvider.makeSimpleQuery(this
+                , UserDictContentProvider.COLUMS
+                , null
+                , new String [] {""}
+                , null //UserDictContentProvider.ORDER_BY_ASC[0]
+        );
+
+
+        UserDictContentProvider.makeSimpleQuery(this
+                , UserDictContentProvider.COLUMS
+                , UserDictionary.Words.WORD + " = ?"
+                , new String [] {"algoritmo"}
+                , null
+        );
+
     }
 }
--- XploreDevice/app/src/main/java/slidingpanellayout/xplore/com/br/xploredevice/utils/UserDictContentProvider.java
+++ XploreDevice/app/src/main/java/slidingpanellayout/xplore/com/br/xploredevice/utils/UserDictContentProvider.java
@@ -1,8 +1,69 @@
 package slidingpanellayout.xplore.com.br.xploredevice.utils;
 
+import android.content.Context;
+import android.database.Cursor;
+
+import android.net.Uri;
+import android.provider.UserDictionary;
+import android.util.Log;
+
+import static android.provider.UserDictionary.Words;
+
 /**
  * Created by r028367 on 26/01/2018.
+ *
+ * Uris de conteudo contem um nome que identifica o conjunto de informacoes que se quer abrigar
+ * (como um nome de uma tabela), esse nome e chamado de AUTHORITY veja em {@link UserDictionary#AUTHORITY}
+ * e um nome que aponta para uma tabela veja {@link UserDictionary.Words#CONTENT_URI}
+ *
  */
 
 public class UserDictContentProvider {
+
+    public static final String [] COLUMS = { Words.WORD, Words.FREQUENCY, Words.LOCALE, Words.APP_ID, Words._ID };
+
+    public static final String ORDER_BY_DESC [] = new String [COLUMS.length];
+    public static final String ORDER_BY_ASC []  = new String [COLUMS.length];
+
+    static {
+        for (int i = 0; i < COLUMS.length ; i++) {
+            ORDER_BY_DESC[i] = COLUMS[i] + " DESC";
+            ORDER_BY_ASC[i] = COLUMS[i] + " ASC";
+        }
+    }
+
+    public static Cursor makeSimpleQuery(Context context, Uri uri, String [] projection
+            , String selectionFilterWhere, String [] selectionArgs, String sortOrder) {
+        return context.getContentResolver().query(uri, projection, selectionFilterWhere, selectionArgs, sortOrder);
+    }
+
+    public static void makeSimpleQuery(Context context
+            , String [] projection, String selectionFilterWhere, String [] selectionFilterArgs, String sortOrder) {
+        Cursor cursor = makeSimpleQuery(
+              context
+            , UserDictionary.Words.CONTENT_URI
+            , projection
+            , selectionFilterWhere
+            , selectionFilterArgs
+            , sortOrder
+        );
+
+        if(cursor != null && cursor.moveToFirst()) {
+            int countResults = cursor.getCount();
+            Log.i("CURSOR_RESULT", String.valueOf(countResults));
+            while ( cursor.moveToNext() ) {
+                String word = cursor.getString(cursor.getColumnIndexOrThrow(COLUMS[0]));
+                long frequency = cursor.getLong(cursor.getColumnIndexOrThrow(COLUMS[1]));
+                String locale = cursor.getString(cursor.getColumnIndexOrThrow(COLUMS[2]));
+                int appId = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMS[3]));
+            }
+        }
+
+        else if(cursor != null){
+            int countResults = cursor.getCount();
+            Log.i("CURSOR_RESULT", String.valueOf(countResults));
+        }
+    }
+
+
 }
