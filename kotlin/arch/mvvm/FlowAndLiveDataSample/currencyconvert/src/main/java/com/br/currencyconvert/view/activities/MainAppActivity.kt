cc41d857c07ebe8c337ea30c3e7b62f4b92fe3db
christoffer lucas <christoffer.santos@mercadolivre.com>
2021-11-24 04:20:30 -0300

    estudos

---
+++ kotlin/arch/mvvm/FlowAndLiveDataSample/currencyconvert/src/main/java/com/br/currencyconvert/view/activities/MainAppActivity.kt
@@ -0,0 +1,12 @@
+package com.br.currencyconvert.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.experience.sample.currencyconvert.R
+
+class MainAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_app)
+    }
+}
